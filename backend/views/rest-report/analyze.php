ff538905a44d954a453a6114026fb535e924c319
Peng Sun <sunnycamel@gmail.com>
2016-09-11 09:26:01 +0800

    minor change

--- backend/views/rest-report/analyze.php
+++ backend/views/rest-report/analyze.php
@@ -11,17 +11,12 @@ $this->params['breadcrumbs'][] = ['label' => 'Report', 'url' => ['view', 'id'=>$
 $this->params['breadcrumbs'][] = $this->title;
 
 AppAsset::register($this); 
-//$this->registerJsFile('@web/js/pdfobject.min.js',['position' => POS_HEAD,'depends'=>['backend\assets\AppAsset']]);  
-//$this->registerCssFile('@web/css/ionicons.min.css',['depends'=>['backend\assets\AppAsset']]); 
-
-
 
 $sqliteUrl = str_replace('/primerbean/media/', 'user/', $model->snpsqlite);
 $sqliteUrl = Yii::$app->params['erp_url'] . $sqliteUrl ;
  
 $data = file_get_contents($sqliteUrl);
 $data = json_decode($data);
-//$data = array('apple', 'orange', 'banana', 'strawberry');
 $data = json_encode($data);
 ?>
 
