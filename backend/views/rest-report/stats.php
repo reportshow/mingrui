49896c66b5b34b4c0ad795bb7329f7582fa05488
Peng Sun <sunnycamel@gmail.com>
2016-09-19 00:24:19 +0800

    improve the UI of report stats

--- backend/views/rest-report/stats.php
+++ backend/views/rest-report/stats.php
@@ -11,65 +11,6 @@ $this->params['breadcrumbs'][] = ['label' => 'Report', 'url' => ['view', 'id'=>$
 $this->params['breadcrumbs'][] = $this->title;
 
 AppAsset::register($this);
-
-/* $sqliteUrl = str_replace('/primerbean/media/', 'user/', $model->snpsqlite); */
-/* $sqliteUrl = Yii::$app->params['erp_url'] . $sqliteUrl ; */
-/* $data = file_get_contents($sqliteUrl); */
-/* $data = json_decode($data, true); */
-
-/* for($i =0;$i<6;$i++){ */
-/*      $text = trim($data[0][$i]); */
-/*      if($text == '') */
-/*      { */
-/*           echo "没有值"; */
-/*      } */
-/*      else { */
-/*           echo ($text); */
-/*      } */
-/*      echo '<br/>'; */
-/* } */
-
-/* echo(trim($data[0][6][0])); */
-/* echo('--' . trim($data[0][6][1])); */
-/* echo('--' . trim($data[0][6][2]). '<br/>'); */
-/* for($i =7;$i<14;$i++){ */
-/*      $text = trim($data[0][$i]); */
-/*      if($text == '') */
-/*      { */
-/*           echo "没有值"; */
-/*      } */
-/*      else { */
-/*           echo ($text); */
-/*      } */
-/*      echo '<br/>'; */
-/* } */
-
-/* echo(trim($data[0][14][0])); */
-/* echo('--' . trim($data[0][14][1]). '<br/>'); */
-
-/* echo('NG16070056--' . trim($data[0][15]['NG16070056'][0])); */
-/* echo('--' . trim($data[0][15]['NG16070056'][1]). '<br/>'); */
-
-/* echo(trim($data[0][16][0])); */
-/* echo('--' . trim($data[0][16][1])); */
-/* echo('--' . trim($data[0][16][2]). '<br/>'); */
-
-/* for($i =17;$i<20;$i++){ */
-/*      echo trim($data[0][$i]) .'<br/>'; */
-/* } */
-
-/* echo(trim($data[0][20][0])); */
-/* echo('--' . trim($data[0][20][1]). '<br/>'); */
-
-/* echo(trim($data[0][21][0][0])); */
-/* echo('--' . trim($data[0][21][0][1])); */
-/* echo('--' . trim($data[0][21][0][2]). '--'); */
-/* echo(trim($data[0][21][1][0])); */
-/* echo('--' . trim($data[0][21][1][1])); */
-/* echo('--' . trim($data[0][21][1][2]). '--'); */
-/* echo(trim($data[0][21][2][0])); */
-/* echo('--' . trim($data[0][21][2][1])); */
-/* echo('--' . trim($data[0][21][2][2]). '<br/>'); */
 ?>
 <script>
   var data = <?php echo $data ?>;
@@ -125,7 +66,7 @@ for(var i=0; i< data_length; i++) {
     area[1] = width / (max-min)*(data[i].end-data[i].start);//width
     area[2] = data[i].count;// text for count
     area[4] = data[i].start.toString().concat('--', data[i].end.toString());//text for coordination
-    area[5] = i*10;//height offset
+    area[5] = (i+1)*10;//height offset
     areas[i] = area;
 }
 
@@ -133,10 +74,10 @@ countmax = areas[0][2];
 countmin = areas[0][2];
 for(var i=0; i<areas.length; i++) {
     if(areas[i][2] >countmax) {
-	countmax = areas[i][2]; 
+	countmax = areas[i][2];
     }
     if(areas[i][2] <countmin) {
-	countmin = areas[i][2]; 
+	countmin = areas[i][2];
     }
 }
 
@@ -190,10 +131,15 @@ function drawArea(contextO, start, width, count, text, heightoff, fillstyle) {
     var textWidth = metrics.width;
     context.fillText(count, start+width/2, startY+(height+10)/2);
     context.font = '10pt Calibri';
-    context.textAlign = "center";
+    context.textAlign = "left";
     context.fillStyle = "#000";
     var metrics = context.measureText(text);
     var textWidth = metrics.width;
-    context.fillText(text, start+width/2, startY+height+20+ heightoff);
+    context.save();
+    context.translate(start+width/2, startY+height+20);
+    context.rotate(-Math.PI / 4);
+    context.textAlign = 'right';
+    context.fillText(text, 0, 0);
+    context.restore();
 }
 </script>
