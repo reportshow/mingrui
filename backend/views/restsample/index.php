5c42aadadc40562b68a76edecd8e9060087cde61
ding,chengliang <goodook@163.com>
2016-10-09 11:51:22 +0800

    医院搜索

--- backend/views/restsample/index.php
+++ backend/views/restsample/index.php
@@ -1,46 +1,58 @@
 <?php
 
-use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\RestSampleSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = '患者资料';
+$this->title                   = '患者资料';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="rest-sample-index">
- 
+
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('新建患者资料', ['create'], ['class' => 'btn btn-success']) ?>
+        <?=Html::a('新建患者资料', ['create'], ['class' => 'btn btn-success'])?>
     </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'rowOptions'   => function ($model) {
-            $url = Yii::$app->urlManager->createUrl(['restsample/view', 'id' => $model->sample_id]);
-            return ['onclick' => "location.href='$url';", 'style'=>'cursor:pointer'];
-        },
-        'emptyCell'=>'搜索',
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+    <?=GridView::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel'  => $searchModel,
+    'rowOptions'   => function ($model) {
+        $url = Yii::$app->urlManager->createUrl(['restsample/view', 'id' => $model->sample_id]);
+        return ['onclick' => "location.href='$url';", 'style' => 'cursor:pointer'];
+    },
+    'emptyCell'    => '搜索',
+    'columns'      => [
+        [
+            'class'   => 'yii\grid\SerialColumn',
+            'options' => ['width' => 50]],
 
-            //'sample_id',
-            'name',
-            //'type',
-            //'ypkd_id',
-            //'barcode',
-         ['attribute'=>'sex',
-         'options'=>['width'=>30],
-          'value'=>function ($model){
-            return  $model->sex =='female' ? '女' :'男';
-         }],
-             'birthday',
-            // 'age',
-           [
+        //'sample_id',
+
+        [
+            'attribute' => 'name',
+            'options'   => ['width' => 150],
+        ],
+        //'type',
+        //'ypkd_id',
+        //'barcode',
+        [
+            'attribute' => 'sex',
+            'options'   => ['width' => 60],
+            'value'     => function ($model) {
+                return $model->sex == 'female' ? '女' : '男';
+            },
+        ],
+         [
+            'attribute' => 'birthday',
+            'options'   => ['width' => 120],
+        ],
+
+        // 'age',
+        [
             'attribute' => 'tel1',
             'label'     => '联系方式',
             'value'     => function ($model) {
@@ -49,59 +61,59 @@ $this->params['breadcrumbs'][] = $this->title;
                 //return str_replace(' ', '', $list[0]) . (count($list) > 1 ? '-等' : '');
                 $tels = str_replace(' ', '', $tels);
                 $tels = str_replace('-', '', $tels);
-                if (strlen($tels) > 11) {
+               /* if (strlen($tels) > 11) {
                     $tels = substr($tels, 0, 11) . '...';
-                }
+                }*/
                 return $tels;
             },
             'filter'    => Html::activeTextInput($searchModel, 'tel1', [
                 'class' => 'form-control',
             ]),
         ],
-            // 'tel2',
-            // 'email:email',
-              'address',
-            // 'symptom:ntext',
-            // 'date',
-            // 'has_project',
-            // 'report_type',
-            // 'guanlian',
-            // 'pdf',
-            // 'has_symptom',
-            // 'relation',
-            // 'related_sid',
-            // 'xianzhengzhe',
-            // 'yangbenruku',
-            // 'heshuanruku',
-            // 'heshuanruku2',
-            // 'yangbenweizi',
-            // 'heshuanweizi',
-            // 'heshuanweizi2',
-            // 'note:ntext',
-            // 'doctor_id',
-            // 'family_id',
-            // 'sales_id',
-            // 'shenhe_status',
-            // 'clinic_no',
-            // 'nationality',
-            // 'patient_no',
-            // 'clinic_symptom:ntext',
-            // 'report_template',
-            // 'created',
-            // 'xiedai',
-            // 'updated',
-            // 'timestamp:ntext',
-            // 'dengji_note:ntext',
-            // 'express',
-            // 'express_no',
-            // 'shouyang_date',
-            // 'shouyanged',
+        // 'tel2',
+        // 'email:email',
+        'address',
+        // 'symptom:ntext',
+        // 'date',
+        // 'has_project',
+        // 'report_type',
+        // 'guanlian',
+        // 'pdf',
+        // 'has_symptom',
+        // 'relation',
+        // 'related_sid',
+        // 'xianzhengzhe',
+        // 'yangbenruku',
+        // 'heshuanruku',
+        // 'heshuanruku2',
+        // 'yangbenweizi',
+        // 'heshuanweizi',
+        // 'heshuanweizi2',
+        // 'note:ntext',
+        // 'doctor_id',
+        // 'family_id',
+        // 'sales_id',
+        // 'shenhe_status',
+        // 'clinic_no',
+        // 'nationality',
+        // 'patient_no',
+        // 'clinic_symptom:ntext',
+        // 'report_template',
+        // 'created',
+        // 'xiedai',
+        // 'updated',
+        // 'timestamp:ntext',
+        // 'dengji_note:ntext',
+        // 'express',
+        // 'express_no',
+        // 'shouyang_date',
+        // 'shouyanged',
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+        ['class' => 'yii\grid\ActionColumn'],
+    ],
+]);?>
 </div>
 <style type="text/css">
     .content-wrapper{overflow: auto}
     .disabled{background: #999;border:0px;}
-</style> 
+</style>
