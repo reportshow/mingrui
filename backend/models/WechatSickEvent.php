7b431df083d29cea5e0e78ae3f6cd7edf94def37
vnool <vnool@email.com>
2016-12-05 22:05:12 +0800

    菜单域名更新

--- backend/models/WechatSickEvent.php
+++ backend/models/WechatSickEvent.php
@@ -11,23 +11,23 @@ class WechatSickEvent extends WechatEvent
 
     public function work_flow()
     {
-        $article = [
-            ['title'      => '检测流程',
+         $article = [
+            ['title'      => '进入首页',
                 'description' => '检测的每个步骤须知',
-                'picurl'      => 'http://ding.scicompound.com/mingrui/report/backend/web/images/1.png',
-                'url'         => 'http://ding.scicompound.com/mingrui/report/backend/web/index.php?r=mingrui-qa'],
-            ['title'      => '检测流程',
-                'description' => '检测的每个步骤须知',
-                'picurl'      => 'http://ding.scicompound.com/mingrui/report/backend/web/images/2.png',
-                'url'         => 'http://ding.scicompound.com/mingrui/report/backend/web/index.php?r=mingrui-qa'],
-            ['title'      => '服务承诺',
-                'description' => '精准服务',
-                'picurl'      => 'http://ding.scicompound.com/mingrui/report/backend/web/images/3.png',
-                'url'         => 'http://ding.scicompound.com/mingrui/report/backend/web/index.php?r=mingrui-qa'],
-            ['title'      => '联系我们',
+                'picurl'      => 'http://www.mono-mr.com/backend/web/images/qa-1.jpg',
+                'url'         => 'http://www.mono-mr.com/backend/web/index.php'],
+            ['title'      => '查看报告',
+                'description' => '查看报告',
+                'picurl'      => 'http://www.mono-mr.com/backend/web/images/qa-2.jpg',
+                'url'         => 'http://www.mono-mr.com/backend/web/?r=wechat/my-report'],
+            ['title'      => '上传图片',
+                'description' => '上传图片',
+                'picurl'      => 'http://www.mono-mr.com/backend/web/images/qa-3.jpg',
+                'url'         => 'http://www.mono-mr.com/backend/web/?r=wechat/my-upload'],
+            ['title'      => '联系方式',
                 'description' => '联系方式，欢迎反馈',
-                'picurl'      => 'http://ding.scicompound.com/mingrui/report/backend/web/images/user1-128x128.jpg',
-                'url'         => 'http://ding.scicompound.com/mingrui/report/backend/web/index.php?r=mingrui-qa'],
+                'picurl'      => 'http://www.mono-mr.com/backend/web/images/qa-4.jpg',
+                'url'         => 'http://www.mono-mr.com/backend/web/index.php?r=mingrui-qa%2Fviewcontact&id=1'],
         ];
         return $this->reply->article($article);
     }
